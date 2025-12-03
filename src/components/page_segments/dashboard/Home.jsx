5f8aeaed7d46319c1659403e7479c7c75f11f32f
NiTzuA <11v35faustin@gmail.com>
2025-12-03 17:06:38 +0800

    Add Status Card

--- src/components/page_segments/dashboard/Home.jsx
+++ src/components/page_segments/dashboard/Home.jsx
@@ -1,14 +1,22 @@
 import Text from "../../ui/Text";
 import Card from "../../ui/Card";
+import Button from "../../ui/Button";
 import { useState, useEffect } from "react";
 import updatesDummy from "../../../data/updates.json";
+import calculationDummy from "../../../data/calculations.json"
+
 
 function Home() {
 
-    const [events, setEvents] = useState([]);
+    const [updates, setUpdates] = useState([]);
+    const [calculations, setCalculations] = useState([]);
 
     useEffect(() => {
-        setEvents(updatesDummy);
+        setCalculations(calculationDummy);
+    }, []);
+
+    useEffect(() => {
+        setUpdates(updatesDummy);
     }, []);
 
     return (
@@ -34,7 +42,7 @@ function Home() {
                                 <div className="flex flex-row">
                                     <Text>Latest Updates</Text>
                                 </div>
-                                {events.map (evt => (
+                                {updates.map (evt => (
                                     <div key={evt.id} className="flex flex-row gap-5">
                                         <div className="flex-grow text-left">
                                             <Text className="font-normal text-xs">{evt.description}</Text>
@@ -48,7 +56,22 @@ function Home() {
                             </div>
                         </Card>
                         <Card>
-
+                            <div className="flex flex-col gap-4">
+                                <div className="flex flex-row">
+                                    <Text>Status</Text>
+                                </div>
+                                <div className="flex flex-row gap-5 justify-between">
+                                    {calculations.map (calc => (
+                                    <div key={calc.id} className="flex flex-row gap-3">
+                                        <img src={calc.image} className="w-14 h-14"></img>
+                                        <div className="flex flex-col justify-start items-start">
+                                            <Text className="font-bold">{calc.name}</Text>
+                                            <Text className="text-xl !text-accent">{calc.value}</Text>
+                                        </div>
+                                    </div>
+                                    ))}
+                                </div>
+                            </div>
                         </Card>
                         <div className="flex flex-row gap-3 justify-between flex-grow">
                             <Card className="flex-grow">
