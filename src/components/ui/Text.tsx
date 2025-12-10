ebae7da4f6732720f79892b312fc49e612e128a5
NiTzuA <11v35faustin@gmail.com>
2025-12-10 10:04:52 +0800

    Convert Text.jsx to TypeScript

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -1,10 +1,20 @@
 const variants = {
     normal: "text-textgray font-medium tracking-wide",
     title: "text-primary font-bold tracking-wide ",
+} as const;
 
-} // Predefined variants which I will set later
+type Variant = keyof typeof variants;
 
-export default function Text ( {variant = "normal", className = "", ...props} ) {
+interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
+    variant?: Variant;
+    className?: string;
+}
+
+export default function Text ( {
+    variant = "normal", 
+    className = "", 
+    ...props
+}: TextProps ) {
     return (
         <p
             {...props}
